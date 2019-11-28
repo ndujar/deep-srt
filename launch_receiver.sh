360c680a099aea0fade099573fc3e19c29fda3de
Rabindranath <rabindranath.andujar@gmail.com>
2019-11-28 14:34:21 +0100

    Adds receiver dockerfile

---
+++ launch_receiver.sh
@@ -0,0 +1,1 @@
+docker build -f dockerfile-base -t base-srt:v1 . && docker build -f dockerfile-receiver -t receiver-srt:v1 . && docker run --volume="$(pwd)/stream":/stream --volume="$(pwd)/logs":/logs --name receiver-srt -p 5000:5000 receiver-srt:v1
