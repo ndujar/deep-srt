6abc0fdc82c39a2283e262ca7671a7afa14bd651
Rabindranath <rabindranath.andujar@gmail.com>
2019-11-28 14:50:11 +0100

    Modifies shared ports in transmitter and receiver

--- launch_transmitter.sh
+++ launch_transmitter.sh
@@ -1,3 +1,3 @@
 docker build -f dockerfile-base -t base-srt:v1 . 
 docker build -f dockerfile-transmitter -t transmitter-srt:v1 . 
-docker run --volume="$(pwd)/stream":/stream --volume="$(pwd)/logs":/logs --device=/dev/video0:/dev/video0 -p 5000:5000 --name transmitter-srt transmitter-srt:v1
+docker run --volume="$(pwd)/stream":/stream --volume="$(pwd)/logs":/logs --device=/dev/video0:/dev/video0 -p 1234:1234 --name transmitter-srt transmitter-srt:v1
