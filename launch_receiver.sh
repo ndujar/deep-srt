820f66f41b5bf53cdf8bcec7f62d0853c3c1cddf
Rabindranath <rabindranath.andujar@gmail.com>
2019-12-17 23:24:24 +0100

    Cleans up docker images

--- launch_receiver.sh
+++ launch_receiver.sh
@@ -1,1 +1,4 @@
-docker build -f dockerfile-base -t base-srt:v1 . && docker build -f dockerfile-receiver -t receiver-srt:v1 . && docker run --volume="$(pwd)/stream":/stream --volume="$(pwd)/logs":/logs --name receiver-srt -p 4201:4201 receiver-srt:v1
+docker rm receiver-srt
+docker build -f dockerfile-base -t base-srt:v1 . 
+docker build -f dockerfile-receiver -t receiver-srt:v1 . 
+docker run --volume="$(pwd)/stream":/stream --volume="$(pwd)/logs":/logs --name receiver-srt -p 4201:4201 receiver-srt:v1
--- launch_transmitter.sh
+++ launch_transmitter.sh
@@ -1,3 +1,4 @@
+docker rm transmitter-srt
 docker build -f dockerfile-base -t base-srt:v1 . 
 docker build -f dockerfile-transmitter -t transmitter-srt:v1 . 
 docker run --volume="$(pwd)/stream":/stream --volume="$(pwd)/logs":/logs --device=/dev/video0:/dev/video0 -p 1234:1234 --name transmitter-srt transmitter-srt:v1
