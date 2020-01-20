0ab4088df7c669465a4228a0fbf5d6e6b365b9dd
Rabindranath <rabindranath.andujar@gmail.com>
2020-01-20 14:32:35 +0100

    Modifies ffmpeg command to get noise proportional to bitrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./srt/build/srt-live-transmit udp://localhost:1234 "$MEDIUM://$RECEIVER_ADDRESS:$RECEIVER_PORT?latency=$LATENCY" -v -stats-report-frequency $REPORT_FREQUENCY -statsout -pf csv &
-exec ffmpeg -f lavfi -i testsrc=duration=300:size=1280x720:rate=30 -c:v mpeg2video -b:v $FFMPEG_BITRATE -f mpegts "udp://127.0.0.1:1234?pkt_size=1316"
+exec ffmpeg -f rawvideo -video_size 1280x720 -pixel_format yuv420p -framerate $FFMPEG_BITRATE -i /dev/urandom -codec:a copy -f mpegts "udp://127.0.0.1:1234?pkt_size=1316"
 
