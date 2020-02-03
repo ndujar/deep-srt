025dfeaf7c427fbb8721fc502328926fe83da970
Maria Sharabayko <msharabayko@haivision.com>
2020-02-03 16:41:02 +0100

    Did some formatting corrections plus deleted not used dependencies

--- streamlit/display.py
+++ streamlit/display.py
@@ -3,50 +3,50 @@ Module to generate an interactive app to visualize and train a QoE predictive mo
 from data retrieved out of srt-live-transmit protocol stats
 It relies of Streamlite library for the visualization and display of widgets
 """
-
-import os.path
-
-from catboost import Pool, CatBoostRegressor, CatBoostClassifier
-
 import pandas as pd
-import streamlit as st
-import numpy as np
-import plotly.express as px
 import plotly.graph_objects as go
-from sklearn import svm
+import streamlit as st
 
-st.title('QoE model predictor')
 
-TRANSMITTER_LOGS = '../logs/transmitter.csv'
-RECEIVER_LOGS = '../logs/receiver.csv'
-FEATURES = ['Time',
-            'pktFlowWindow',
-            'pktCongestionWindow',
-            'pktFlightSize',
-            'msRTT',
-            'mbpsBandwidth',
-            'pktSent',
-            'pktSndLoss',
-            'pktSndDrop',
-            'pktRetrans',
-            'byteSent',
-            'byteSndDrop',
-            'mbpsSendRate',
-            'usPktSndPeriod',
-            'pktRecv',
-            'pktRcvLoss',
-            'pktRcvDrop',
-            'pktRcvRetrans',
-            'pktRcvBelated',
-            'byteRecv',
-            'byteRcvLoss',
-            'byteRcvDrop',
-            'mbpsRecvRate',
-            'pktSndFilterExtra',
-            'pktRcvFilterExtra',
-            'pktRcvFilterSupply',
-            'pktRcvFilterLoss'
-            ]
+# TODO: Change filepaths back
+TRANSMITTER_LOGS = 'logs/transmitter.csv'
+RECEIVER_LOGS = 'logs/receiver.csv'
+# TRANSMITTER_LOGS = '/Users/msharabayko/_data/mac_eunorth/local/4-srt-xtransmit-stats-snd.csv'
+# RECEIVER_LOGS = '/Users/msharabayko/_data/mac_eunorth/msharabayko@40.69.89.21/3-srt-xtransmit-stats-rcv.csv'
+
+
+# TODO: Uncomment features back
+FEATURES = [
+    'Time',
+    # 'pktFlowWindow',
+    # 'pktCongestionWindow',
+    # 'pktFlightSize',
+    'msRTT',
+    'mbpsBandwidth',
+    'pktSent',
+    'pktSndLoss',
+    'pktSndDrop',
+    'pktRetrans',
+    # 'byteSent',
+    # 'byteSndDrop',
+    'mbpsSendRate',
+    # 'usPktSndPeriod',
+    'pktRecv',
+    'pktRcvLoss',
+    'pktRcvDrop',
+    'pktRcvRetrans',
+    'pktRcvBelated',
+    # 'byteRecv',
+    # 'byteRcvLoss',
+    # 'byteRcvDrop',
+    'mbpsRecvRate',
+    # 'pktSndFilterExtra',
+    # 'pktRcvFilterExtra',
+    # 'pktRcvFilterSupply',
+    # 'pktRcvFilterLoss'
+]
+
+
 def load_data(data_uri, nrows):
     """
     Function to retrieve data from a given file or URL
@@ -54,48 +54,52 @@ def load_data(data_uri, nrows):
     nrows limits the amount of data displayed for optimization
     """
     data_df = pd.read_csv(data_uri, nrows=nrows)
-    # lowercase = lambda x: str(x).lower()
-    # data_df.rename(lowercase, axis='columns', inplace=True)
     data_df = data_df[FEATURES]
     return data_df
 
-def plot_scatter(title, x_metric, y_metrics, x_axis_title, y_axis_title, df_aggregated):
+
+def plot_scatter(
+    title,
+    x_metric,
+    y_metrics,
+    x_axis_title,
+    y_axis_title,
+    df_aggregated
+):
     """
     Function to plot and format a scatterplot from the aggregated dataframe
     """
     data = []
     shapes = list()
     for y_metric in y_metrics:
-        data.append(go.Scatter(x=df_aggregated.index,
-                            y=df_aggregated[y_metric],
-                            mode='lines',
-                            marker=dict(
-                                    opacity=0.8,
-                                    line=dict(width=0)
-                                    ),
-                            name=y_metric
-                            
-                            )
-                        )
+        data.append(
+            go.Scatter(
+                x=df_aggregated.index,
+                y=df_aggregated[y_metric],
+                mode='lines',
+                marker=dict(opacity=0.8, line=dict(width=0)),
+                name=y_metric
+            )
+        )
 
     fig = go.Figure(data=data)
-    fig.update_layout(title=title,
-                      xaxis_title=x_axis_title,
-                      yaxis_title=y_axis_title,
-                      legend=go.layout.Legend(x=0,
-                                              y=1,
-                                            traceorder="normal",
-                                            font=dict(family="sans-serif",
-                                                    size=8,
-                                                    color="black"
-                                                    ),
-                                            bgcolor="LightSteelBlue",
-                                            bordercolor="Black",
-                                            borderwidth=2
-                                             ),
-                                             shapes=shapes
-                    )
+    fig.update_layout(
+        title=title,
+        xaxis_title=x_axis_title,
+        yaxis_title=y_axis_title,
+        legend=go.layout.Legend(
+            x=0,
+            y=1,
+            traceorder="normal",
+            font=dict(family="sans-serif", size=8, color="black"),
+            bgcolor="LightSteelBlue",
+            bordercolor="Black",
+            borderwidth=2
+        ),
+        shapes=shapes
+    )
     st.plotly_chart(fig)
+
 
 def plot_correlation_matrix(df_aggregated):
     """
@@ -105,23 +109,22 @@ def plot_correlation_matrix(df_aggregated):
     df_features = pd.DataFrame(df_aggregated[FEATURES])
     corr = df_features.corr()
     corr.style.background_gradient(cmap='coolwarm')
-    fig = go.Figure(data=go.Heatmap(x=FEATURES,
-                                    y=FEATURES,
-                                    z=corr
-                                    ))
- 
+    fig = go.Figure(data=go.Heatmap(x=FEATURES, y=FEATURES, z=corr))
     st.plotly_chart(fig)
+
 
 def main():
     """
     Main function to train and evaluate tamper and QoE models
     """
+    st.title('QoE model predictor')
+
+    st.subheader('Raw features')
+    st.write(FEATURES)
+
     # Get QoE pristine dataset (no attacks)
     df_transmitter = load_data(TRANSMITTER_LOGS, 50000)
     df_receiver = load_data(RECEIVER_LOGS, 50000)
-    
-    st.subheader('Raw features')
-    st.write(FEATURES)
 
     # Preprocess the datasets to align time series and remove spurious data
     # Filter out zero values
@@ -142,44 +145,50 @@ def main():
     df_synchronized['Rate'] = df_synchronized['mbpsSendRate'] / (df_synchronized['mbpsRecvRate'])
 
     # Display datasets
-    st.subheader('Raw TRANSMITTER data')
+    st.subheader('Processed TRANSMITTER data')
     st.write(df_transmitter, df_transmitter.shape)
 
-    st.subheader('Raw RECEIVER data')
+    st.subheader('Processed RECEIVER data')
     st.write(df_receiver, df_receiver.shape)
 
-    st.subheader('Raw SYNCHRONIZED data')
+    st.subheader('SYNCHRONIZED data')
     st.write(df_synchronized, df_synchronized.shape)
 
-    st.subheader('Summary SYNCHRONIZED data')
+    st.subheader('Summary, SYNCHRONIZED data')
     st.write(df_synchronized.describe())
 
-    plot_scatter('TRANSMITTER Time series',
-                 'Time',
-                 df_transmitter.columns,
-                 'Time',
-                 'TRANSMITTER',
-                 df_transmitter)
+    plot_scatter(
+        'TRANSMITTER Time series',
+        'Time',
+        df_transmitter.columns,
+        'Time',
+        'TRANSMITTER',
+        df_transmitter
+    )
 
-    plot_scatter('RECEIVER Time series',
-                 'Time',
-                 df_receiver.columns,
-                 'Time',
-                 'RECEIVER',
-                 df_receiver)
-    
+    plot_scatter(
+        'RECEIVER Time series',
+        'Time',
+        df_receiver.columns,
+        'Time',
+        'RECEIVER',
+        df_receiver
+    )
 
-    plot_scatter('SYNCHRONIZED Time series',
-                 'Time',
-                 df_synchronized.columns,
-                 'Time',
-                 'RECEIVER',
-                 df_synchronized)
-    # Display correlation matrix
+    plot_scatter(
+        'SYNCHRONIZED Time series',
+        'Time',
+        df_synchronized.columns,
+        'Time',
+        'SYNCHRONIZED',
+        df_synchronized
+    )
+
+    # Display correlation matrixs
     plot_correlation_matrix(df_receiver)
     plot_correlation_matrix(df_transmitter)
     plot_correlation_matrix(df_synchronized)
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     main()
